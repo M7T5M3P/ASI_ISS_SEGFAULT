2225eb2763d0dff3c99b243e24dc07ea350f9ca8
Cogal <mathieu.rio@epitech.eu>
2022-12-01 22:04:33 +0100

    fix: game01 not found

--- tree/game/game01/game01.php
+++ tree/game/game01/game01.php
@@ -1,5 +1,5 @@
 <?php
-    include "get_path.php";
+    include "../../../get_path.php";
     $sex_info = new parse_ini();
 ?>
 <!DOCTYPE html>
@@ -14,6 +14,17 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </head>
 <body>
-    <h1>Hello World</h1>
+    <!-- <div class="card" style="width: 22%;margin-left: 39%; margin-top: 10%">
+        <h5 class="card-title">Question</h5>
+        <p class="card-text">Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Donec rutrum congue leo eget malesuada. Vivamus suscipit tortor eget felis porttitor volutpat. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Quisque velit nisi, pretium ut lacinia in, elementum id enim.</p>
+        <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</button>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Separated link</a></li>
+        </ul>
+    </div> -->
 </body>
 </html>
