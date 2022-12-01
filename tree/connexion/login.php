ae2eaca5b55f06fdbca7dbe8b93d0fad2a0b4b8f
NathanCoquelin <nathan.coquelin@epitech.eu>
2022-12-01 21:44:24 +0100

    feat: added styling to elements to resize rem depending on media queries

--- tree/connexion/login.php
+++ tree/connexion/login.php
@@ -1,3 +1,7 @@
+<?php
+    include "../../get_path.php";
+    $sex_info = new parse_ini();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,8 +9,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo $sex_info->get_CSS() ?>" media="screen"/>    
 </head>
 <body>
-    
+    <div class="test"></div>
+    <h1>TEST</h1>
 </body>
 </html>
