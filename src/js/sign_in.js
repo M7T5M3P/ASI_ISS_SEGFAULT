37a9f6509e11652722c65fe34aa2c6d471c7822e
Mathys2003 <mathys.thevenot@epitech.eu>
2022-12-02 02:15:25 +0100

    fix : testing code

--- src/js/sign_in.js
+++ src/js/sign_in.js
@@ -6,25 +6,15 @@ function new_user() {
         var avatar = $("#avatar").val();
         var email = $("#email").val();
 
-        if (username == "")
-            alert("Aucun username !");
-        else if (password == "")
-            alert("Aucun mot de passe!");
-        else if (avatar == "")
-            alert("Aucun mot de passe!");
-        else if (avatar == "")
-            alert("Aucun mot de passe!");
-        else {
-            //user.php ? username = mathys & email=tmp4 & avatar=tmp3 & password=tyo & signin=2 & tmp_pwd=
-            $.get("./user.php", { 'username': username, 'password': password, 'email': email, 'avatar': avatar, 'signin': 0 },
-                function (data) {
-                    alert(data);
-                    if (data == "1") {
-                        location.replace("game/game01/game01.php");
-                    } else {
-                        alert("Tu as déja un compte ou une erreur interne c'est produite.");
-                    }
-                });
-        }
+        //user.php ? username = mathys & email=tmp4 & avatar=tmp3 & password=tyo & signin=2 & tmp_pwd=
+        $.get("./user.php", { 'username': username, 'password': password, 'email': email, 'avatar': avatar, 'signin': 0 },
+            function (data) {
+                alert(data);
+                if (data == "1") {
+                    location.replace("game/game01/game01.php");
+                } else {
+                    alert("Tu as déja un compte ou une erreur interne c'est produite.");
+                }
+            });
     });
 }
