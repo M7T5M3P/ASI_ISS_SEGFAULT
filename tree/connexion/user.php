4630469102ff62d922bf44a5191d7f5ea7ec0f36
Mathys2003 <mathys.thevenot@epitech.eu>
2022-12-02 06:59:14 +0100

    feat : testing code

--- tree/connexion/user.php
+++ tree/connexion/user.php
@@ -2,7 +2,7 @@
 header('Content-Type: text/html; charset=utf-8');
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
-include 'init_class_user.php';
+include './init_class_user.php';
 
 $username = $_GET["username"];
 $email = $_GET["email"];
