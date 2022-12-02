29fbaef4a646bd95f03644ca3807897a87847483
NathanCoquelin <nathan.coquelin@epitech.eu>
2022-12-02 03:07:41 +0100

    did sign up with icons and cool design

--- tree/connexion/login.php
+++ tree/connexion/login.php
@@ -24,8 +24,10 @@
             <div class="login-portal__header">
                 <span>Sex Info</span>
             </div>
-         <form method='post'>
-              <div class="login-portal__interface">
+     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+     <path fill="#219EBC" fill-opacity="1" d="M0,192L26.7,197.3C53.3,203,107,213,160,197.3C213.3,181,267,139,320,144C373.3,149,427,203,480,213.3C533.3,224,587,192,640,192C693.3,192,747,224,800,229.3C853.3,235,907,213,960,176C1013.3,139,1067,85,1120,106.7C1173.3,128,1227,224,1280,229.3C1333.3,235,1387,149,1413,106.7L1440,64L1440,0L1413.3,0C1386.7,0,1333,0,1280,0C1226.7,0,1173,0,1120,0C1066.7,0,1013,0,960,0C906.7,0,853,0,800,0C746.7,0,693,0,640,0C586.7,0,533,0,480,0C426.7,0,373,0,320,0C266.7,0,213,0,160,0C106.7,0,53,0,27,0L0,0Z" style="--darkreader-inline-fill: #007acc;"></path>
+                       </svg>
+            <div class="login-portal__interface">
                     <div class='login-portal__interface--message'>
                         <h1>Login</h1>
                     </div>
@@ -55,7 +57,6 @@
                         <a class='login-portal__interface--options--createAccount' href='createAccount.php'>Create an Account</a><br>
                     </div>   
                 </div>
-            </form>
          </div>
      </div>
 </body>
--- tree/connexion/signup.php
+++ tree/connexion/signup.php
@@ -19,7 +19,7 @@ $sex_info = new parse_ini();
     <div class="return_btn">
         <a href='/index.html'>
             <svg class=return_btn__icon>
-                <use xlink:href="<?php echo $sex_info->get_ICONS() ?>/sprite.svg#icon-chevron-left"></use>
+                <use xlink:href="<?php echo $sex_info->get_ICONS() ?>sprite.svg#icon-chevron-left"></use>
             </svg>
         </a>
     </div>
@@ -28,6 +28,9 @@ $sex_info = new parse_ini();
             <div class="login-portal__header">
                 <span>Sex Info</span>
             </div>
+     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+  <path fill="#219EBC" fill-opacity="1" d="M0,128L26.7,138.7C53.3,149,107,171,160,165.3C213.3,160,267,128,320,117.3C373.3,107,427,117,480,149.3C533.3,181,587,235,640,256C693.3,277,747,267,800,224C853.3,181,907,107,960,90.7C1013.3,75,1067,117,1120,154.7C1173.3,192,1227,224,1280,213.3C1333.3,203,1387,149,1413,122.7L1440,96L1440,0L1413.3,0C1386.7,0,1333,0,1280,0C1226.7,0,1173,0,1120,0C1066.7,0,1013,0,960,0C906.7,0,853,0,800,0C746.7,0,693,0,640,0C586.7,0,533,0,480,0C426.7,0,373,0,320,0C266.7,0,213,0,160,0C106.7,0,53,0,27,0L0,0Z" style="--darkreader-inline-fill: #007acc;"></path>
+</svg>
             <form onsubmit="event.preventDefault(); new_user(); return false;">
                 <div class="login-portal__interface">
                     <div class=' login-portal__interface--message'>
@@ -35,27 +38,27 @@ $sex_info = new parse_ini();
                     </div>
                     <div class='login-portal__interface--input'>
                         <div class="login-portal__interface--input--avatar">
-                            <label for="email-input">
+                            <label for="avatar">
                                 <svg class="login-portal__interface--input--avatar--icon">
-                                    // <use xlink:href="../img/sprint.svg#icon-envelope-o"></use>
+                                    <use xlink:href="<?php echo $sex_info->get_ICONS() ?>sprite.svg#icon-camera"></use>
                                 </svg>
                             </label>
                             <input class='login-portal__interface--input--avatar--input' id='avatar' type='text'
                                 name='avatar' placeholder='Avatar' required>
                         </div>
                         <div class="login-portal__interface--input--email">
-                            <label for="email-input">
+                            <label for="emailt">
                                 <svg class="login-portal__interface--input--email--icon">
-                                    // <use xlink:href="../img/sprint.svg#icon-envelope-o"></use>
+                                     <use xlink:href="<?php echo $sex_info->get_ICONS() ?>sprite.svg#icon-envelope"></use>
                                 </svg>
                             </label>
                             <input class='login-portal__interface--input--email--input' id='email' type='email'
                                 name='email' placeholder='Email' required>
                         </div>
                         <div class="login-portal__interface--input--username">
-                            <label for="email-input">
+                            <label for="username">
                                 <svg class="login-portal__interface--input--username--icon">
-                                    // <use xlink:href="../img/sprint.svg#icon-envelope-o"></use>
+                                     <use xlink:href="<?php echo $sex_info->get_ICONS() ?>sprite.svg#icon-user"></use>
                                 </svg>
                             </label>
                             <input class='login-portal__interface--input--username--input' id='username' type='text'
@@ -64,7 +67,7 @@ $sex_info = new parse_ini();
                         <div class="login-portal__interface--input--password">
                             <label for="password-input">
                                 <svg class="login-portal__interface--input--password--icon">
-                                    // <use xlink:href="../img/sprint.svg#icon-lock1"></use>
+                                     <use xlink:href="<?php echo $sex_info->get_ICONS() ?>sprite.svg#icon-lock-closed"></use>
                                 </svg>
                             </label>
                             <input class='login-portal__interface--input--password--input' id='password' type='password'
