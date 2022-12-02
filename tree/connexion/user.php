6d52f2de266ec0264e39250eff783399c717785f
Mathys2003 <mathys.thevenot@epitech.eu>
2022-12-02 07:03:31 +0100

    feat : testing code

--- tree/connexion/user.php
+++ tree/connexion/user.php
@@ -15,7 +15,7 @@ if (isset($_GET["tmp_pwd"])) {
     echo $user->html;
 } else if ($_GET["signin"] == 0) {
     $user = new User($signin, $username, $password, $email, $avatar, null);
-    echo $user->return;
+    echo $user->tmp_password;
 } else {
     $result = new User($signin, null, $password, $email, null, null);
     echo $result->return;
