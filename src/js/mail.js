d6d1d8f7fe48eab6c1a726ceab52c27fdef629dd
Mathys2003 <mathys.thevenot@epitech.eu>
2022-12-02 07:36:48 +0100

    feat : testing code

--- src/js/mail.js
+++ src/js/mail.js
@@ -1,11 +1,10 @@
 function check_password() {
     $("#password").click(function () {
-        alert("hi");
         var password = $("#password2").val();
         var email = $("#email").val();
-        alert(password + email);
         $.get("./check_email_password.php", { 'password': password, 'email': email },
             function (data) {
+                alert(data);
                 if (data == "1") {
                     alert("Connected !");
                     location.replace("game/game01?email=" + email);
